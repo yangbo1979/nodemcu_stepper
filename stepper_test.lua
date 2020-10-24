7b05afcf52b6ff96b11d2dc354451616a568d4f9
yangbo <gyangbo@gmail.com>
2020-10-24 11:10:39 +0800

    Update stepper_test.lua

--- stepper_test.lua
+++ stepper_test.lua
@@ -1,7 +1,7 @@
 stepper  = require ('stepper')
 stepper.init()
-desired_steps = 2500
-interval = 1
+desired_steps = 1500
+interval = 1200
 print('stepper.rotate() - start')
 stepper.rotate(stepper.FORWARD,desired_steps,interval,function ()
     print('Rotation done. inside callback.')
